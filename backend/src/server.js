c3e2781493d2a7dba81019dd3bd648eb987b9b71
namoura <130542021+a-namoura@users.noreply.github.com>
2025-12-26 18:43:32 +0100

    Initialise backend

---
+++ backend/src/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get("/api/health", (req, res) => {
+  res.json({ status: "API running" });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Backend running on http://localhost:${PORT}`);
+});
