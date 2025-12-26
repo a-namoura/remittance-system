51d94e54d7666f07b758c4a4e6d2c8fc037844a1
namoura <130542021+a-namoura@users.noreply.github.com>
2025-12-26 20:33:28 +0100

    Update transactionRoutes.js

--- backend/src/routes/transactionRoutes.js
+++ backend/src/routes/transactionRoutes.js
@@ -3,7 +3,7 @@ import { protect } from "../middleware/authMiddleware.js";
 
 export const transactionRouter = express.Router();
 
-// Placeholder for Week 6/7 features
+// Placeholder
 transactionRouter.get("/", protect, (req, res) => {
   res.json({ message: "Transaction endpoints coming soon" });
 });
