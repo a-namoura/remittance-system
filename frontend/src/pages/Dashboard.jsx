9677ce0e6ec7f1fe23624e2f2a0b3cc91930b18f
namoura <130542021+a-namoura@users.noreply.github.com>
2026-01-16 14:00:23 +0100

    Update Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { apiRequest } from "../services/api.js";
+import ConnectWalletButton from "../components/ConnectWalletButton.jsx";
 
 export default function Dashboard() {
   const [me, setMe] = useState(null);
@@ -11,10 +12,14 @@ export default function Dashboard() {
       setError("No token found. Please login.");
       return;
     }
+    
+    apiRequest("/api/me", { token })
+    .then((data) => {
+      console.log("ME response", data);
+      setMe(data.user);
+    })
+    .catch((err) => setError(err.message));
 
-    apiRequest("/api/users/me", { token })
-      .then((data) => setMe(data.user))
-      .catch((err) => setError(err.message));
   }, []);
 
   function logout() {
@@ -37,6 +42,7 @@ export default function Dashboard() {
           <button className="mt-4 bg-gray-900 text-white px-4 py-2 rounded" onClick={logout}>
             Logout
           </button>
+          <ConnectWalletButton />
         </div>
       ) : !error ? (
         <div>Loading...</div>
