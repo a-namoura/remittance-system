2b9debb027b1d5381f6fa19510476daefcde919b
namoura <130542021+a-namoura@users.noreply.github.com>
2026-01-20 16:25:01 +0100

    Update walletRoutes.js

--- backend/src/routes/walletRoutes.js
+++ backend/src/routes/walletRoutes.js
@@ -4,9 +4,4 @@ import { linkWallet } from "../controllers/walletController.js";
 
 export const walletRouter = express.Router();
 
-// Placeholder
-walletRouter.get("/", protect, (req, res) => {
-  res.json({ message: "Wallet endpoints coming soon" });
-});
-
 walletRouter.post("/link", protect, linkWallet);
