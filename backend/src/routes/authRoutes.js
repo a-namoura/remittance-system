98b0d1918565e19012832323c2123f748cf374dc
namoura <130542021+a-namoura@users.noreply.github.com>
2026-01-23 13:54:32 +0100

    Update authRoutes.js

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,7 +1,9 @@
 import express from "express";
-import { register, login } from "../controllers/authController.js";
+import { register, login, logout } from "../controllers/authController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 export const authRouter = express.Router();
 
 authRouter.post("/register", register);
 authRouter.post("/login", login);
+authRouter.post("/logout", protect, logout);
